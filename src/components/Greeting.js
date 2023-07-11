923393aa8cc7cc0e100161c8109f811647b58a10
Stephen-Adom <stephen.alaska94@hotmail.com>
2023-07-11 10:45:55 +0000

    add greeting component for displaying random greeting message

---
+++ src/components/Greeting.js
@@ -0,0 +1,28 @@
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { useQuery } from 'react-query';
+import { setGreeting } from '../store/features/greetingSlice';
+
+const Greeting = () => {
+  const greeting = useSelector((state) => state.greeting.greeting);
+  const dispatch = useDispatch();
+
+  const { data, isLoading } = useQuery(['greeting'], () => fetch('http://localhost:3000/random-greeting').then((res) => res.json()));
+
+  useEffect(() => {
+    if (data && !isLoading) {
+      dispatch(setGreeting(data));
+    }
+  }, [data]);
+
+  return (
+    <div className="Greeting">
+      <h1>
+        Greeting:
+        {greeting.title}
+      </h1>
+    </div>
+  );
+};
+
+export default Greeting;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { QueryClientProvider, QueryClient } from 'react-query';
+import store from './store/store';
 import App from './App';
+import Greeting from './components/Greeting';
 import reportWebVitals from './reportWebVitals';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <Greeting />,
+      },
+    ],
+  },
+]);
+
+const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <Provider store={store}>
+        <RouterProvider router={router} />
+      </Provider>
+    </QueryClientProvider>
   </React.StrictMode>,
 );
 
